4ca1b716b6be929365dcdbb7192c1ee15dbf3a7b
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2016-07-31 18:08:52 +0200

    Remove deprecated method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
   protect_from_forgery with: :exception
-  before_filter :redirect_subdomain
+  before_action :redirect_subdomain
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
