16fe10007e454de415fbaac6b287379334cfeb75
Katharina Ehrhardt <kadtmail@gmail.com>
2016-07-31 19:00:13 +0200

    enforce user name during registration

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 20160731121603) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
