9aae3801f21d707fb778c9d1ab6a9bc06d573492
Katharina Ehrhardt <kadtmail@gmail.com>
2016-07-31 16:55:23 +0200

    add sign up and login

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    if current_user
+      render :status
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-
   root 'welcome#index'
+
+  get 'signup'  => 'users#new'
+  resources :users
+
+  get 'login'  => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
