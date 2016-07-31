634dc229c6138f583f99fee348ddd6020bc722c7
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2016-07-31 19:13:07 +0200

    Fix database migration
    The migration is broken and must be fixed. This will require additional
    work on the server.

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 20160731121603) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
