3eaccfb12e28ffb47afe5b0e6dd27140cd2be450
Katharina Ehrhardt <kadtmail@gmail.com>
2016-09-25 10:43:38 +0200

    set all districts as checked by clicking on all

--- app/assets/javascripts/nurseries.js
+++ app/assets/javascripts/nurseries.js
@@ -1,4 +1,14 @@
-$('.nursery-filter input').change(function () {
+$('.nursery-filter input[name = "district[]"]').change(function () {
+  $('.nursery-filter').submit();
+});
+
+$('.nursery-filter input[name = all]').change(function () {
+  if ($(this).is(':checked')) {
+    $('.nursery-filter input[name = "district[]"]').prop('checked', true);
+  }
+  else {
+    $('.nursery-filter input[name = "district[]"]').prop('checked', false);
+  }
   $('.nursery-filter').submit();
 });
 
