8bc3ade19b7d91bfa509e662fa5e1033bd33b757
Katharina Ehrhardt <kadtmail@gmail.com>
2016-10-21 16:00:34 +0200

    save new child in the regstration and connect child data with theparents

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -6,11 +6,17 @@ class RegistrationController < ApplicationController
 
   def create
     @child = Child.create(
-      first_name: params[:first_name],
-      last_name: params[:first_name],
+      first_name: params[:child_first_name],
+      last_name: params[:child_last_name],
       birth_date: params[:birth_date],
+      parents: current_parents
     )
+
     nursery_id = params[:nursery_id]
     redirect_to "/registration/#{nursery_id}/confirmation"
+  end
+
+  def confirmation
+    @nursery = Nursery.find(params[:id])
   end
 end
