8f9ed5a1471274838baf35242add37700cffe435
Katharina Ehrhardt <kadtmail@gmail.com>
2016-10-21 17:59:23 +0200

    validate first_name of child

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -5,15 +5,20 @@ class RegistrationController < ApplicationController
   end
 
   def create
-    @child = Child.create(
+    @child = Child.new(
       first_name: params[:child_first_name],
       last_name: params[:child_last_name],
       birth_date: params[:birth_date],
       parents: current_parents
     )
-
-    nursery_id = params[:nursery_id]
-    redirect_to "/registration/#{nursery_id}/confirmation"
+    if @child.save
+      session[:child_id] = @child.id
+      nursery_id = params[:nursery_id]
+      redirect_to "/registration/#{nursery_id}/confirmation"
+    else
+      @nursery = Nursery.find(params[:nursery_id])
+      render 'index'
+    end
   end
 
   def confirmation
--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,9 @@
 class Child < ApplicationRecord
   belongs_to :parents
+
+  validates :first_name, presence: {message: 'darf nicht fehlen'}
+  validates :last_name, presence: {message: 'darf nicht fehlen'}
+
+#  validates_format_of :birth_date, :with => /\d{2}\/\d{2}\/\d{4}/, message: "Das Datum muss wie folgt eingetragen werden: tt/mm/jjjj"
+#  validates :birth_date, presence: true, message: "Das Datum fehlt!"
 end
