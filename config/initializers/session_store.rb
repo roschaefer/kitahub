6efb356e3c14a0768743e8d8b03c9c84b6ab91ad
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2016-10-30 15:09:26 +0100

    Make redis URL configurable
    On the production system, the redis URL will be set via an environment
    variable.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-redis_url = 'redis://localhost:6379/0/session'
+redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379'
 Rails.application.config.session_store :redis_store,
                                        key: '_kitahub_session',
                                        expires_in: 1.day,
-                                       servers: redis_url
+                                       servers: "#{redis_url}/0/session"
