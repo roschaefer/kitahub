6c5f39ba4e60a58a503009adde9d72ac286b023d
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2016-10-30 17:50:42 +0100

    Add security to session cookies
    The session will expire in redis, but it is also good, when the cookie
    expires as well. Reducing the sending of cookies across sites, will also
    increase the security.
    
    HTTPS is already enforced by nginx, but enforcing TLS in rails will also
    ensure HTTPS only cookies.

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Kitahub
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Ensure HTTPS only
+    config.force_ssl = true if Rails.env.production?
+
     # Use UUIDs as primary keys instead of sequence numbers
     config.active_record.primary_key = :uuid
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,5 +3,7 @@
 redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379'
 Rails.application.config.session_store :redis_store,
                                        key: '_kitahub_session',
+                                       expires: 1.day.from_now,
+                                       same_site: 'Lax',
                                        expires_in: 1.day,
                                        servers: "#{redis_url}/0/session"
