e906067f08b2c00c7207cc7911a6853798b355cb
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2016-10-30 14:59:45 +0100

    Use redis as session store
    Sessions will now be stored in redis. They will also expire in
    redis. Thus clean up happens automatically.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_kitahub_session'
+redis_url = 'redis://localhost:6379/0/session'
+Rails.application.config.session_store :redis_store,
+                                       key: '_kitahub_session',
+                                       expires_in: 1.day,
+                                       servers: redis_url
