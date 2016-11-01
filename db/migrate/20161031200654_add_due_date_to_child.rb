c67e19421adb555d37a396b0ee02be0ba9f28f33
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2016-11-01 06:12:04 +0100

    Fix code styles

--- db/migrate/20161031200654_add_due_date_to_child.rb
+++ db/migrate/20161031200654_add_due_date_to_child.rb
@@ -1,3 +1,4 @@
+# Adds a date to the children table to which the child wants to register.
 class AddDueDateToChild < ActiveRecord::Migration[5.0]
   def change
     add_column :children, :prefered_start_date, :date
