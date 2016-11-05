dd4551e942966e4de32871b6d6d40a5e70324635
Jan Ehrhardt <jan.ehrhardt@gmail.com>
2016-11-05 15:52:52 +0100

    Remove old select nursery logic
    The first idea was, that parents select a nursery to start a
    registration process. The reality is, that they do a first request to
    the nursery instead. Thus the new implementation is in the nursery
    class.

--- app/models/parents.rb
+++ app/models/parents.rb
@@ -4,15 +4,6 @@ class Parents < ApplicationRecord
   has_many :registrations
   has_many :children
 
-  def select_nursery(child, nursery)
-    registration = Registration.create(
-      parents: self,
-      child: child,
-      nursery: nursery
-    )
-    NurseryMailer.nursery_selected(registration).deliver_later
-  end
-
   delegate :email, to: :user
 
   def display_name
--- spec/models/parents_spec.rb
+++ spec/models/parents_spec.rb
@@ -9,18 +9,4 @@ RSpec.describe Parents, type: :model do
     expect(first_parents.valid?).to eq true
     expect(second_parents.valid?).to eq false
   end
-
-  it 'should have a registration after selecting a nursery' do
-    address = Address.new('Park Avenue 4 A', '12345', 'Berlin')
-    nursery = Nursery.create(
-      name: 'foo',
-      address: address,
-      phone: '875958',
-      mail: 'foo@bar.org'
-    )
-    user = User.create(email: 'foo@bar.com', password: 'secretfoo')
-    parents = Parents.create(user: user)
-    parents.select_nursery(nursery)
-    expect(parents.registrations.count).to eq 1
-  end
 end
